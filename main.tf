bc437182fdbe6acaaf2cfbe72e410e762433d6a8
mariohak <hakramam@gmail.com>
2026-02-04 17:11:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "dev_vpc" {
     name     = "CapstoneVPC"
   }
 }
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
