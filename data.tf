512c337965b2258f52549c7b0a82e83edc98a0d5
259325312+mariohak@users.noreply.github.com <259325312+mariohak@users.noreply.github.com>
2026-02-19 12:02:49 +0200

    Fetch latest Amazon Linux 2 AMI using SSM parameter

---
+++ data.tf
@@ -0,0 +1,5 @@
+// Fetch the latest Amazon Linux 2 AMI ID from AWS Systems Manager Parameter Store
+
+data "aws_ssm_parameter" "al2_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
