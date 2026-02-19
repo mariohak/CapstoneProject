9b89c5a488166002fc853642102415aeb27abd2e
259325312+mariohak@users.noreply.github.com <259325312+mariohak@users.noreply.github.com>
2026-02-19 10:52:54 +0200

    Define project and infrastructure variables

---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "project_name" {
+  type    = string
+  default = "capstone-project"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-west-2a", "us-west-2b"]
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.2.0/24", "10.0.4.0/24"]
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "vockey"
+}
+
+variable "ssh_ingress_cidr" {
+  type        = string
+  description = "Your public IP in CIDR form, e.g. 1.2.3.4/32"
+  default     = "0.0.0.0/0"
+}
+
+variable "db_name" {
+  type    = string
+  default = "wordpress"
+}
+
+variable "db_username" {
+  type    = string
+  default = "wordpressuser"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_multi_az" {
+  type        = bool
+  description = "Sandbox-safe default is false (lab restricts standby / Multi-AZ). Set true only if allowed."
+  default     = false
+}
